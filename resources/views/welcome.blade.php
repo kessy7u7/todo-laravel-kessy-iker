462aad78ef8b858004e78b712dd333b00c70f1cf
kessy7u7 <kessvan.one@gmail.com>
2023-01-15 22:34:43 +0100

    Cambio de título 1.0 - V.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
         <a href="/">Home</a>
 
         {{-- create todo --}}
-        <x-card title="To-Do Application 2.1">
+        <x-card title="To-Do Application by Iker y Kessy">
             <form
                 action="{{ $model->exists ? route('todo.update', $model->id) : route('todo.store') }}"
                 method="POST"
